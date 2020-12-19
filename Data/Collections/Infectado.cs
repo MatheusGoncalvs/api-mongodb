8049a93547559e6c5a165f4dc245561bb31d57f8
Matheus Goncalves <matheusgoncalvs@hotmail.com>
2020-12-19 12:47:19 -0300

    adicionado o driver mongo and collection initial

---
+++ Data/Collections/Infectado.cs
@@ -0,0 +1,21 @@
+using System;
+using MongoDB.Driver.GeoJsonObjectModel;
+
+namespace api-mongodb.data.collections
+{
+    public class Infectado
+    {
+        public int UserId { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+
+        public Infectado(int userId, DateTime dataNascimento, string sexo, double latitude, double longitude)
+        {
+            this.UserId = userId;
+            this.DataNascimento = dataNascimento;
+            this.Sexo = sexo;
+            this.Localizacao = new GeoJson2DGeographicCoordinates(longitude, latitude);
+        }        
+    }
+}
